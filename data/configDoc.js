9debf80cdd1e8d4e5508068f960d455d7f88a4bd
luongvantien1194 <68776377+luongvantien1194@users.noreply.github.com>
2026-04-28 14:54:37 +0700

    Update configDoc.js

--- data/configDoc.js
+++ data/configDoc.js
@@ -70,27 +70,86 @@ window.DOC_CONFIG = {
     {
       key: "kanji1",
       file: "kanji1",
-      type: "pdf",
+      type: "md",
       label: "Kanji 1"
     },
     {
       key: "kanji2",
       file: "kanji2",
-      type: "pdf",
+      type: "md",
       label: "Kanji 2"
+    },
+    {
+      key: "kanji3",
+      file: "kanji3",
+      type: "md",
+      label: "Kanji 3"
+    },
+    {
+      key: "kanji4",
+      file: "kanji4",
+      type: "md",
+      label: "Kanji 4"
+    },
+    {
+      key: "kanji5",
+      file: "kanji5",
+      type: "md",
+      label: "Kanji 5"
+    },
+    {
+      key: "kanji6",
+      file: "kanji6",
+      type: "md",
+      label: "Kanji 6"
+    },
+    {
+      key: "kanji7",
+      file: "kanji7",
+      type: "md",
+      label: "Kanji 7"
+    },
+    {
+      key: "kanji8",
+      file: "kanji8",
+      type: "md",
+      label: "Kanji 8"
+    },
+    {
+      key: "kanji9",
+      file: "kanji9",
+      type: "md",
+      label: "Kanji 9"
+    },
+    {
+      key: "kanji10",
+      file: "kanji10",
+      type: "md",
+      label: "Kanji 10"
+    },
+    {
+      key: "kanji11",
+      file: "kanji11",
+      type: "md",
+      label: "Kanji 11"
+    },
+    {
+      key: "kanji12",
+      file: "kanji12",
+      type: "md",
+      label: "Kanji 12"
+    },
+    {
+      key: "kanji13",
+      file: "kanji13",
+      type: "md",
+      label: "Kanji 13"
     },
     {
       key: "gra",
       file: "gra",
       type: "xlsx",
       label: "tai lieu excel"
-    }
-    ,
-    {
-      key: "img",
-      file: "img.jpg",
-      type: "img",
-      label: "Anh A"
     }
   ]
 };
