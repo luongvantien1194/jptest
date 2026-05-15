860169bf94283343972706a9557a1b96befad664
luongvantien1194 <68776377+luongvantien1194@users.noreply.github.com>
2026-05-15 16:21:37 +0700

    Update sw.js

--- pip-kanji-pwa/sw.js
+++ pip-kanji-pwa/sw.js
@@ -1,5 +1,5 @@
 /* eslint-disable no-restricted-globals */
-var CACHE_NAME = "kanji-v23";
+var CACHE_NAME = "kanji-v24";
 var ASSETS = ["./index.html", "./script.js", "./manifest.json", "../data/kanjiData.js"];
 
 self.addEventListener("install", function (event) {
