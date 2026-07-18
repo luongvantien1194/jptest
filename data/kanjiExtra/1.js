24cebd71617286f4e059d581da22c0d382b11a06
tien <luongvantien1194@gmail.com>
2026-07-18 12:22:08 +0700

    Add kanji

---
+++ data/kanjiExtra/1.js
@@ -0,0 +1,4 @@
+window._kanjiExtra = window._kanjiExtra || [];
+window._kanjiExtra.push(
+  { "stt": 1, "level": "n3", "kanji": "1232132", "hanviet": "13213", "on_reading": "", "kun_reading": "", "stroke_count": "", "radicals": "", "core_meaning": "123213", "story_image": "", "logic_development": "", "memory_tip": "", "vocabulary": "" },
+);
